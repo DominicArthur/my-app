4de2a5469eda33aca1e2f2537cd313ee36aff442
DominicArthur <g00407252@atu.ie>
2023-12-21 20:31:03 +0000

    Added comments and fixed layout

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,49 +1,65 @@
 // Home.js
 
+// Import the 'useState' hook from React
 import { useState } from "react";
 
- function Home(){
-
+// Functional component for the Home page
+function Home() {
+  // State variables to store form input values
   const [title, setTitle] = useState('');
   const [poster, setPoster] = useState('');
   const [director, setDirector] = useState('');
 
-  const handleSubmit = (i) => {
-    i.preventDefault();
+  // Function to handle form submission
+  const handleSubmit = (e) => {
+    // Prevent the default form submission behavior
+    e.preventDefault();
 
+    // Log the form input values
     console.log(title, poster, director);
   }
 
-  return(
+  // JSX structure for the Home component
+  return (
     <div>
-      <form submit={handleSubmit}>
+      {/* Form for adding a new movie */}
+      <form onSubmit={handleSubmit}>
+        {/* Input field for movie title */}
         <div className="my-form-group">
-        <label>Movie Title: </label>
-        <input type="text"
-         className="my-form-control" 
-          value={title}
-          onChange={(i) => { setTitle(i.target.value) }}
+          <label>Movie Title: </label>
+          <input
+            type="text"
+            className="my-form-control"
+            value={title}
+            onChange={(e) => { setTitle(e.target.value) }}
           />
-          </div>
-          <div className="my-from-group"> 
+        </div>
+        {/* Input field for movie poster */}
+        <div className="my-from-group">
           <label>Movie Poster: </label>
-          <input type="text"
+          <input
+            type="text"
             className="my-form-control"
             value={poster}
-            onChange={(i) => { setPoster(i.target.value) }}
-    	    />
-          </div>
-          <div className="my-form-group">
-            <label>Director's Name: </label>
-            <input type="text"
-              className="my-form-control"
-              value={director}
-              onChange={(i) => {setDirector(i.target.value) }}
-            />
-          </div>
-          <input type="submit" value="Add Movie"></input>
+            onChange={(e) => { setPoster(e.target.value) }}
+          />
+        </div>
+        {/* Input field for director's name */}
+        <div className="my-form-group">
+          <label>Director's Name: </label>
+          <input
+            type="text"
+            className="my-form-control"
+            value={director}
+            onChange={(e) => { setDirector(e.target.value) }}
+          />
+        </div>
+        {/* Submit button */}
+        <input type="submit" value="Add Movie" />
       </form>
     </div>
-  )
- }
+  );
+}
+
+// Export the Home component as the default export
 export default Home;
